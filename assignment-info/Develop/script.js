cc434d0693369c4ce5b11d2027fdc8593f69074a
thebadams <bpeteradams@gmail.com>
2021-02-23 06:47:29 -0500

    added some basic variables and objects to describe the generated password

--- assignment-info/Develop/script.js
+++ assignment-info/Develop/script.js
@@ -1,5 +1,29 @@
 // Assignment Code
+// Set Global Variables
+
 var generateBtn = document.querySelector("#generate");
+
+var characterTypes = {
+  upperCase: ["A,", "B", "C", "D", "E", "F"],
+  lowerCase: ["a", "b", "c", "d", "e", "f"],
+  numeric: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+  specialCharacters: //insert array of special characters here.
+
+};
+
+var passwordInfo = {
+  passwordLength: 0,
+  includesUpperCase: false,
+  includesLowerCase: false,
+  includesNumberic: false,
+  includesSpecialCharacters: false,
+  passwordCharacters: []
+
+};
+
+function generatePassword(){
+  // code to generate password
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +35,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
